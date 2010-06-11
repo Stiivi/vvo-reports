0267c58f34e9e7a4140f66db91a3e182be8a9975
Stiivi <stefan@agentfarms.net>
2010-06-11 14:06:42 +0200

    added report by supplier; fixes

--- sandbox/report.rb
+++ sandbox/report.rb
@@ -9,12 +9,19 @@ class Report
 
 def run
     Brewery::load_default_configuration
-    DataMapper::Logger.new($stdout, :debug)
+    # DataMapper::Logger.new($stdout, :debug)
+    # DataMapper::Logger.new('log', :debug)
+    DataObjects::Postgres.logger = DataObjects::Logger.new('sql.log', :debug) 
     
     initialize_model
     
     time = Time.now
-    top_level_report([2009])
+    # top_level_report([2009])
+    report_obstaravatel([2010], '30416094')
+
+#    for i in 0..100
+#        speed_test([2009])
+#    end
     puts "Elapsed time: #{Time.now - time}"
 end
 
@@ -49,25 +56,42 @@ def top_level_report(report_date)
 
     
     ################################################################
-    # Top 10 orgs
+    # Add computed slice
+    
+    slice.add_computed_field(:podiel) { |record|
+        record[:sum] / hodnota_zmluv
+    }
 
-    dim = @cube.dimension_with_name (:dodavatel)
-    level = dim.level_with_name(:organisation)
-    desc_field = level.description_field.to_sym
-t = Time.now
-    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => dim, 
+    ################################################################
+    # Top 10 dodavatel
+
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => :dodavatel, 
     			                        :row_levels => [:organisation],
     			                        :limit => :rank,
     			                        :limit_value => 10,
     			                        :limit_sort => :top})
 
-puts "ELAPSED: #{Time.now - t}"
-
     puts
-    puts "== #{dim.label} '#{desc_field}'"
+    puts "== Dodavatel"
 
     result.each { |row|
-        puts "-- #{row[desc_field]}: #{row[:sum].to_f} #{row[:record_count]}"
+        puts "-- #{row[:name]}: #{row[:sum].to_f} #{row[:record_count]} #{row[:podiel]}"
+    }
+
+    ################################################################
+    # Top 10 orgs - obstaravatel
+
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => :obstaravatel, 
+    			                        :row_levels => [:organisation],
+    			                        :limit => :rank,
+    			                        :limit_value => 10,
+    			                        :limit_sort => :top})
+
+    puts
+    puts "== Obstaravatel"
+
+    result.each { |row|
+        puts "-- #{row[:name]}: #{row[:sum].to_f} #{row[:record_count]} #{row[:podiel]}"
     }
 
     ################################################################
@@ -120,26 +144,101 @@ puts "ELAPSED: #{Time.now - t}"
     }
 
     return
+end
 
-    slice = slice.slice(:cpv, ["48"])
-    result = slice.aggregate(:zakazka_hodnota, [:sum])
-    puts "SLICE SUM: #{result[:sum].to_f} #{result[:record_count]}"
+def report_obstaravatel(report_date, obstaravatel_ico)
 
-    slice = @cube.slice(:date, [2009])
-    # results = slice.drill_down_aggregate(:dodavatel, :region, :zakazka_hodnota, [:sum])
-    results = top10_hodnota(slice, :dodavatel, :organisation)
+    ################################################################
+    # Main slice
+
+    slice = @cube.whole.cut_by_point(:date, report_date)
+    slice = slice.cut_by_point(:obstaravatel, [:all, obstaravatel_ico])
     
-    results.each { |result|
-        puts "| #{result[:ico]} #{result[:name]} #{result[:sum].to_f} #{result[:record_count]} |"
+    rows = slice.aggregate(:zmluva_hodnota)
+    result = rows[0]
+
+    hodnota_zmluv = result[:sum].to_f
+    pocet_zmluv = result[:record_count]
+
+    puts "\n== Summary"
+    puts "-- Hodnota zmluv: #{hodnota_zmluv}"
+    puts "-- Pocet zmluv  : #{pocet_zmluv}"
+
+    
+    ################################################################
+    # Add computed slice
+    
+    slice.add_computed_field(:podiel) { |record|
+        record[:sum] / hodnota_zmluv
     }
 
-    results.each { |result|
-        puts "| #{result[:ico]} #{result[:name]} #{result[:sum].to_f} #{result[:record_count]} |"
+    ################################################################
+    # Top 10 dodavatel
+
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => :dodavatel, 
+    			                        :row_levels => [:organisation],
+    			                        :limit => :rank,
+    			                        :limit_value => 10,
+    			                        :limit_sort => :top})
+
+    puts
+    puts "== Dodavatel"
+
+    result.each { |row|
+        puts "-- #{row[:name]}: #{row[:sum].to_f} #{row[:record_count]} #{row[:podiel]}"
     }
 
 
-    # puts date_dim.to_yaml
+    ################################################################
+    # CPV
+    dim = @cube.dimension_with_name (:cpv)
+    level = dim.level_with_name(:division)
+    desc_field = level.description_field.to_sym
+    
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => dim, 
+    			                        :row_levels => [:division],
+    			                        :limit => :rank,
+    			                        :limit_value => 10,
+    			                        :limit_sort => :top})
 
+    puts
+    puts "== #{dim.label} '#{desc_field}'"
+
+    result.each { |row|
+        puts "-- #{row[desc_field]}: #{row[:sum].to_f} #{row[:record_count]}"
+    }
+
+
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => :druh_postupu, 
+    			                        :row_levels => [:druh_postupu]})
+
+    puts "\n"
+    puts "== Druh postupu"
+
+    result.each { |row|
+        puts "-- #{row[:druh_postupu]}: #{row[:sum].to_f} #{row[:record_count]}"
+    }
+
+    
+    puts "\n"
+    puts "== Poslednych 12 mesiacov"
+
+    current_date = Date.today
+    to_date_id = Dimension::date_key(current_date)
+    from_date_id = Dimension::date_key(current_date << 12)
+    
+    year_slice = slice.dup
+    year_slice.remove_cuts_by_dimension(:date)
+    year_slice = year_slice.cut_by_range(:date, from_date_id, to_date_id)
+    
+    result = year_slice.aggregate(:zmluva_hodnota, {:row_dimension => :date, 
+    			                        :row_levels => [:year, :month]})
+    
+    result.each { |row|
+        puts "-- #{row[:year]} #{row[:month_sname]}: #{row[:sum].to_f} #{row[:record_count]}"
+    }
+
+    return
 end
 
 def top10_hodnota(slice, dimension, level)
