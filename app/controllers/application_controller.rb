30c2da13899c691e974ff2308cde3c2ebb1a4a1e
Vojto <vojto@zion.local>
2010-06-17 23:07:18 +0200

    added tables and pie charts as data views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  protected
+  
+  def find_cube
+    Brewery::load_rails_configuration
+    @connection = Brewery::data_store_manager.create_connection(:default)
+
+
+    @model = Brewery::Model.first(:name => "vvo")
+    @cube = @model.cubes.first( :name => "vvo" )
+    table = @connection[@cube.fact_table.to_sym]
+    @cube.dataset = Brewery::Dataset.dataset_from_database_table(table)
+  end
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,60 +8,21 @@ class ReportsController < ApplicationController
   
   include Brewery
   
+  before_filter :find_cube
+
   def index
-    Brewery::load_rails_configuration
-    @connection = Brewery::data_store_manager.create_connection(:default)
-
-
-    @model = Brewery::Model.first(:name => "vvo")
-    @cube = @model.cubes.first( :name => "vvo" )
-
-    # FIXME: make this nicer
-    table = @connection[@cube.fact_table.to_sym]
-    @cube.dataset = Brewery::Dataset.dataset_from_database_table(table)
-
-
-    # TOP 10 @report
-
-    @report = Hash.new
-
-
-    # Main slice
-
     slice = @cube.whole.cut_by_point(:date, [2009])
-
-    rows = slice.aggregate(:zmluva_hodnota)
-    result = rows[0]
-
-    hodnota_zmluv = result[:sum].to_f
-    pocet_zmluv = result[:record_count]
-
-    @report[:hodnota_zmluv] = hodnota_zmluv
-    @report[:pocet_zmluv] = pocet_zmluv
+    result = slice.aggregate(:zmluva_hodnota)[0]
+    
+    @hodnota_zmluv = result[:sum].to_f
+    @pocet_zmluv = result[:record_count]
 
     slice.add_computed_field(:podiel) { |record|
-      record[:sum] / hodnota_zmluv
+      record[:sum] / @hodnota_zmluv
     }
 
-    ################################################################
-    # Top 10 dodavatel
-
-    table = top_10_dodavatelia(slice)
-
-    presenter = DataTablePresenter.new
-    presenter.format_column(0) { | cell |
-    "<a href='dodavatel/#{cell.value}'>#{cell.formatted_value}</a>"
-    }
-    @report[:top_dodavatelia] = table
-
-    presenter = DataTablePresenter.new
-    presenter.format_column(0) { | cell |
-    "<a href='obstaravatel/#{cell.value}'>#{cell.formatted_value}</a>"
-    }
-
-    @report[:presenter] = presenter
-    @table = presenter.present_as_html(@report[:top_dodavatelia])
-
+    @top_10_dodavatelia = top_10_dodavatelia(slice)
+    @top_10_obstaravatelia = top_10_obstaravatelia(slice)
   end
   
   def top_10_dodavatelia(slice)
@@ -79,6 +40,25 @@ class ReportsController < ApplicationController
       result.each { |row|
           table.add_row([[row[:ico], row[:name]], row[:sum], row[:podiel]])
       }
-      return table
+      
+      table
+  end
+  
+  def top_10_obstaravatelia(slice)
+    result = slice.aggregate(:zmluva_hodnota, {:row_dimension => :obstaravatel, 
+    			                        :row_levels => [:organisation],
+    			                        :limit => :rank,
+    			                        :limit_value => 10,
+    			                        :limit_sort => :top})
+
+    table = DataTable.new
+    table.add_column(:text, "Obstarávateľ", :org)
+    table.add_column(:currency, "Suma", :suma, {:precision => 0, :currency => '€', :alignment => :right})
+    table.add_column(:percent, "Podiel", :podiel, { :precision => 2 , :alignment => :right} )
+    
+    result.each { |row|
+        table.add_row([[row[:ico], row[:name]], row[:sum], row[:podiel]])
+    }
+    return table
   end
 end
