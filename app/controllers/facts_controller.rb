00343d0e944495d16dbe624b4f08f31832877b69
Stiivi <stefan@agentfarms.net>
2010-06-27 23:15:14 +0200

    added kriteria vyhodnotenia; detail mockup

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,10 +1,22 @@
 class FactsController < ApplicationController
+  before_filter :initialize_model
+
   def show
     @id = params[:id]
-    @test_hash = {
-      :lorem => "ipsum",
-      :dolor => "sit",
-      :amet => "hm?"
-    }
+    
+    @fact = @cube.fact(@id)
+
+    @view_fields = [ 'vestnik_cislo', 
+                     'zakazka_nazov',
+                     'zmluva_hodnota',
+                     'cpv.cpv_code',
+                     'dodavatel.name',
+                     'dodavatel.region',
+                     'dodavatel.address',
+                     'obstaravatel.name',
+                     'obstaravatel.ico',
+                     'obstaravatel.address',
+                     'obstaravatel.account_sector',
+                     'druh_postupu' ]
   end
 end
