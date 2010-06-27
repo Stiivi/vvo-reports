6a6d50f670695c9ce312032ea5fe976bc7b4c4bc
Stiivi <stefan@agentfarms.net>
2010-06-28 00:40:34 +0200

    added more fact mockups; added slovak locale + configured rails to use SK locale by default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
-  
+
+def link_to_cut(dimension_name, label, path)
+    # FIXME: normalize elements - encode ',' in path values
+    path_str = path.join(',')
+    cut = "cut=#{dimension_name}:#{path_str}"
+    link = link_to(label, cut)
+
+    return link
+end
 end
