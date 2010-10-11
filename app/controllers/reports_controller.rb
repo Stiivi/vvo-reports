6783e5bfe2ab47d735bb8c09adf64413919b8295
Vojto <vojto@rinik.net>
2010-10-11 08:49:14 +0200

    Feature by fungovala ale Stiivi to pokazil tak robim branch a az vstane ta sa ho spytam ze o co mu islo.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -62,6 +62,7 @@ class ReportsController < ApplicationController
     show_report = @param_report.delete(:show_report)
     unless show_report.blank?
       slicer = Brewery::CubeSlicer.new(@cube)
+      raise @param_report.to_yaml
       @param_report.each do |dimension_name, value|
         if params[:current_cut]
           slicer.update_from_param(params[:current_cut])
@@ -70,7 +71,13 @@ class ReportsController < ApplicationController
         slicer.update_from_param(param)
       end
       respond_to do |wants|
-        path = report_path(:all, :cut => slicer.to_param)
+        if params[:current_path]
+          path = params[:current_path] + "?cut=" + slicer.to_param
+          puts "GENERATING PATH"
+          puts path
+        else
+          path = report_path(:all, :cut => slicer.to_param)
+        end
         wants.html { return redirect_to path }
         wants.js { return render :text => "window.location = '#{path}'" }
       end
@@ -88,6 +95,7 @@ class ReportsController < ApplicationController
       search = SphinxSearch.new(query, dimension)
 
       # FIXME: @vojto why? at least put notice on site
+      #        ^ It's more fun when they don't know.
       search.limit = 50
       search.process
       @result_counts[dimension.name.to_sym] = search.total_found
@@ -95,6 +103,7 @@ class ReportsController < ApplicationController
         sanitized_path = CGI::escape(result[:path].to_s)
         result[:path] = sanitized_path
         # FIXME: verify this (changed by @stiivi)
+        
         result
       end
     end
