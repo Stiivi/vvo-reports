4d36de3103959d0172088b2da0fe857258abdb95
Vojto <vojto@rinik.net>
2010-10-29 16:16:49 +0200

    Ordering search results on Sphinx level again. Removing hack that I had before. Warning: Reindex Sphinx index or it will crash.

--- lib/sphinx_search.rb
+++ lib/sphinx_search.rb
@@ -18,19 +18,16 @@ class SphinxSearch
   def process
     client = Riddle::Client.new
     
-    # So here's the hack. After two hours of fun I still
-    # couldn't get Sphinx sorting to work. 
-    # So I'm gonna get all IDs (but max 10k) from Sphinx and let the database
-    # do the sorting.
-    client.limit = 10000
+    client.offset = @offset
+    client.limit = @limit
     
-    # For the case I'll ever want to get this sorting to work:
-    # if @order == "alphabet"
-    #   client.sort_mode = :attr_asc
-    #   client.sort_by = 'description_value'
-    # end
+    if @order == "alphabet"
+      client.sort_mode = :attr_asc
+      client.sort_by = 'description_value_ordinal'
+    end
     
     result = client.query(@query)
+    
     document_ids = result[:matches].collect do |match|
       match[:doc]
     end
@@ -38,22 +35,19 @@ class SphinxSearch
     connection = Brewery::workspace.connection
     dataset = connection[:idx_dimensions]
     
-    query = dataset.filter(:id => document_ids).limit(@limit, @offset)
+    query = dataset.filter(:id => document_ids) #.limit(@limit, @offset)
+    # Problem here. Order at two places. This is because Sphinx
+    # returns list of IDs which are in correct order, but SQL won't
+    # use that order and it will mess it up. So I have to reorder 
+    # by description value again.
+    # I have no idea what happens when I use default ordering option
+    # called "Relevance". I suppose as long as client thinks it's Relevance
+    # it doesn't matter that it's actually random mess.
     if @order == "alphabet"
       query = query.order_by(:description_value)
     end
     @results = query.all
-    @total_found = result[:total_found]
-
-    # I'm gonna keep this piece around for a while. I'm pretty sure
-    # sure some bug was getting resolved by this piece, I just don't
-    # know which one. Anyway, I'm pretty sure client will let us know
-    # if there's still a problem and I'll be happy to uncomment this
-    # again in that case.
-    #
-    #     query = @query.force_encoding('utf-8')
-    #     converted_query = Iconv.conv('us-ascii//translit', 'utf-8', query).gsub(/'/, '')
-    
+    @total_found = result[:total_found]    
   end
   
 end
